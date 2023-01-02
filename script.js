30b3ad7ae6d49b394ed9fcaf047e545ef749776a
keshab4855 <keshabroka123@gmail.com>
2023-01-02 21:40:54 +1100

    arrays

--- script.js
+++ script.js
@@ -189,3 +189,138 @@
 //   console.log(`Juice with ${apple}apples and ${orange}oranges`);
 // };
 // fruitProcessor(2, 3);
+
+////////////////////////////CODING CHALLENGE 1////////////////////////////
+
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+// gymnastics discipline, which works differently.
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so
+// one average score per team).
+// A team only wins if it has at least double the average score of the other team.
+// Otherwise, no team wins!
+// Your tasks:
+// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+// 2. Use the function to calculate the average for both teams
+// 3. Create a function 'checkWinner' that takes the average score of each team
+// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+// to the console, together with the victory points, according to the rule above.
+// Example: "Koalas win (30 vs. 13)"
+// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+// Data 2
+// 5. Ignore draws this time
+// Test data:
+// § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+// § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+// Hints:
+// § To calculate average of 3 values, add them all together and divide by 3
+// § To check if number A is at least double number B, check for A >= 2 * B.
+// Apply this to the team's average scores �
+
+////////////////////////////CODING CHALLENGE 1 SOLUTION////////////////////////////
+
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// let avgDolphins = calcAverage(85, 54, 41);
+// console.log(`Dolphin average is ${avgDolphins}`);
+// let avgKoalas = calcAverage(23, 34, 27);
+// console.log(`Koalas average is ${avgKoalas}`);
+
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins wins ${avgDolphins} vs ${avgKoalas}`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas wins (${avgDolphins} vs ${avgKoalas})`);
+//   } else {
+//     console.log("No team wins");
+//   }
+//   return;
+// };
+
+// checkWinner(avgDolphins, avgKoalas);
+
+////////////////////////////ARRAYS////////////////////////////
+
+// let friends = ["keshb", "hari", "Ram"];
+// console.log(friends);
+
+// console.log(friends[2]);
+// console.log(friends.length);
+// friends[2] = "Ramesh";
+// console.log(friends);
+
+// const fName = "keshab";
+// const lName = "Roka";
+// const age = 2023 - 1998;
+// const keshab = [fName, lName, age, friends];
+// console.log(keshab.length);
+
+// const calcAge = function (birthYear) {
+//   return 2023 - birthYear;
+// };
+// const years = [1990, 2001, 1993, 1997, 1998];
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[2]);
+// const age4 = calcAge(years[years.length - 1]);
+// const ages = [age1, age2, age3, age4];
+// console.log(ages);
+
+////////////////////////////ARRAYS METHODS////////////////////////////
+// let friends = ["keshb", "hari", "Ram"];
+// friends.push("sham");
+// console.log(friends);
+
+// friends.unshift("Jay");
+// console.log(friends);
+
+// friends.pop();
+// console.log(friends);
+
+// friends.shift();
+// console.log(friends);
+
+// console.log(friends.indexOf("hari"));
+
+////////////////////////////CODING CHALLENGE 2 ////////////////////////////
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+// 20%.
+// Your tasks:
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns
+// the corresponding tip, calculated based on the rules above (you can check out
+// the code from first tip calculator challenge if you need to). Use the function
+// type you like the most. Test the function using a bill value of 100
+// 2. And now let's use arrays! So create an array 'bills' containing the test data
+// below
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from
+// the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+// Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can
+// actually be the returned value of a function! So you can just call a function as array
+// values (so don't store the tip values in separate variables first, but right in the new
+// array) �
+////////////////////////////CODING CHALLENGE 2 SOLUTION////////////////////////////
+
+// const calcTip = (billValue) => {
+//   if (billValue >= 50 && billValue <= 300) {
+//     return (15 / 100) * billValue;
+//   } else {
+//     return (20 / 100) * billValue;
+//   }
+// };
+
+// const calcTip = (billValue) =>
+//   billValue >= 50 && billValue <= 300
+//     ? (15 / 100) * billValue
+//     : (20 / 100) * billValue;
+
+// const bills = [125, 555, 44];
+// const tip1 = calcTip(bills[0]);
+// const tip2 = calcTip(bills[1]);
+// const tip3 = calcTip(bills[2]);
+
+// const tips = [tip1, tip2, tip3];
+// console.log(tips);
+
+// const total = [bills[0] + tip1, bills[1] + tip2, bills[2] + tip3];
+// console.log(total);
