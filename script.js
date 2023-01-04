98ee973489c236e233d68b5375f92ecd4ff53015
keshab4855 <keshabroka123@gmail.com>
2023-01-05 03:43:21 +1100

    opening and closing modal

--- script.js
+++ script.js
@@ -578,5 +578,21 @@
 // });
 
 ///////////////////////////////Modal Project////////////////////////////
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
+document.querySelector(".close-modal").addEventListener("click", () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+console.log(btnsOpenModal);
 
 //////////////////////////////////Modal finished/////////////////////////
