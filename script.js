fb0d3e5f48f93b0f27b2e70ce4f9183419106e68
keshab4855 <keshabroka123@gmail.com>
2023-01-05 02:31:40 +1100

    Modal project start

--- script.js
+++ script.js
@@ -526,53 +526,57 @@
 
 // console.log(document.querySelector(".guess").value);
 
-let number = Math.trunc(Math.random() * 20 + 1);
-let highscore = 0;
+// let number = Math.trunc(Math.random() * 20 + 1);
+// let highscore = 0;
 
-let score = 20;
-document.querySelector(".score").textContent = score;
+// let score = 20;
+// document.querySelector(".score").textContent = score;
 
-document.querySelector(".check").addEventListener("click", () => {
-  const guess = Number(document.querySelector(".guess").value);
-  if (!guess) {
-    document.querySelector(".message").textContent = "No Number !!";
-  } else if (guess > number) {
-    if (score > 0) {
-      score--;
-      document.querySelector(".score").textContent = score;
-      document.querySelector(".message").textContent = "Too high";
-    } else {
-      document.querySelector(".message").textContent = "You lost the game";
-    }
-  } else if (guess < number) {
-    if (score > 0) {
-      score--;
-      document.querySelector(".score").textContent = score;
-      document.querySelector(".message").textContent = "Too low";
-    } else {
-      document.querySelector(".message").textContent = "You lost the game";
-    }
-  } else if (guess === number) {
-    document.querySelector(".number").textContent = number;
-    document.querySelector(".message").textContent =
-      "Correct !! You are a winner";
-    document.querySelector("body").style.backgroundColor = "green";
-    if (score > highscore) {
-      highscore = score;
-      document.querySelector(".highscore").textContent = highscore;
-    }
-  } else {
-    document.querySelector(".message").textContent = "Start guessing....";
-  }
-});
+// document.querySelector(".check").addEventListener("click", () => {
+//   const guess = Number(document.querySelector(".guess").value);
+//   if (!guess) {
+//     document.querySelector(".message").textContent = "No Number !!";
+//   } else if (guess > number) {
+//     if (score > 0) {
+//       score--;
+//       document.querySelector(".score").textContent = score;
+//       document.querySelector(".message").textContent = "Too high";
+//     } else {
+//       document.querySelector(".message").textContent = "You lost the game";
+//     }
+//   } else if (guess < number) {
+//     if (score > 0) {
+//       score--;
+//       document.querySelector(".score").textContent = score;
+//       document.querySelector(".message").textContent = "Too low";
+//     } else {
+//       document.querySelector(".message").textContent = "You lost the game";
+//     }
+//   } else if (guess === number) {
+//     document.querySelector(".number").textContent = number;
+//     document.querySelector(".message").textContent =
+//       "Correct !! You are a winner";
+//     document.querySelector("body").style.backgroundColor = "green";
+//     if (score > highscore) {
+//       highscore = score;
+//       document.querySelector(".highscore").textContent = highscore;
+//     }
+//   } else {
+//     document.querySelector(".message").textContent = "Start guessing....";
+//   }
+// });
 
-document.querySelector(".again").addEventListener("click", () => {
-  score = 20;
-  number = Math.trunc(Math.random() * 20 + 1);
-  document.querySelector(".score").textContent = score;
-  document.querySelector(".message").textContent = "Start guessing....";
-  document.querySelector(".number").textContent = "?";
-  document.querySelector(".guess").value = "";
-  document.querySelector("body").style.backgroundColor = "#222";
-  return;
-});
+// document.querySelector(".again").addEventListener("click", () => {
+//   score = 20;
+//   number = Math.trunc(Math.random() * 20 + 1);
+//   document.querySelector(".score").textContent = score;
+//   document.querySelector(".message").textContent = "Start guessing....";
+//   document.querySelector(".number").textContent = "?";
+//   document.querySelector(".guess").value = "";
+//   document.querySelector("body").style.backgroundColor = "#222";
+//   return;
+// });
+
+///////////////////////////////Modal Project////////////////////////////
+
+//////////////////////////////////Modal finished/////////////////////////
