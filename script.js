0ef48c81d64b272236755bbdb38a41f226f6e2b7
keshab4855 <keshabroka123@gmail.com>
2023-01-07 14:45:09 +1100

    destructuring object

--- script.js
+++ script.js
@@ -670,6 +670,53 @@
 // console.log(newKeshab);
 
 /////////////////////////////////Destructuring arrrays/////////////////
+// const restaurant = {
+//   name: "Classico Italiano",
+//   location: "Via Angelo Tavanti 23, Firenze, Italy",
+//   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+//   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+//   mainMenu: ["Pizza", "Pasta", "Risotto"],
+//   order: function (starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+// };
+
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+// const [x, y, z] = arr;
+// console.log(a, b, c);
+// console.log(x, y, z);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// [main, , secondary] = [secondary, , main];
+// console.log(main, secondary);
+// console.log(restaurant.categories);
+
+// console.log(restaurant.order(2, 0));
+
+// const nestedArr = [2, 4, [5, 6]];
+// const [i, , j] = nestedArr;
+// console.log(i, ...j);
+
+/////////////////////////////////Destructuring objects/////////////////
 const restaurant = {
   name: "Classico Italiano",
   location: "Via Angelo Tavanti 23, Firenze, Italy",
@@ -695,23 +742,12 @@ const restaurant = {
   },
 };
 
-const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
-const [x, y, z] = arr;
-console.log(a, b, c);
-console.log(x, y, z);
+const { name, openingHours, categories } = restaurant;
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
 
-let [main, , secondary] = restaurant.categories;
-// console.log(main, secondary);
-
-[main, , secondary] = [secondary, , main];
-console.log(main, secondary);
-console.log(restaurant.categories);
-
-console.log(restaurant.order(2, 0));
-
-const nestedArr = [2, 4, [5, 6]];
-const [i, , j] = nestedArr;
-console.log(i, ...j);
+let a = 111;
+let b = 2222;
+let obj = { a: 333, b: 444, c: 555 };
+({ a, b } = obj);
+console.log(a, b);
