4df9a010264f241c5989c05a6a177a6899200b80
keshab4855 <keshabroka123@gmail.com>
2023-01-12 03:15:52 +1100

    async/await

--- script.js
+++ script.js
@@ -970,25 +970,37 @@
 // console.log(keshab.__proto__.__proto__);
 
 //////////////////////////////Promise////////////////////////////////////////////
-function func1() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const error = true;
-      if (!error) {
-        console.log("Your Promise has been resolved");
-        resolve();
-      } else {
-        console.log("Your promise has not been resolved");
-        reject("Sorry not fulfilled");
-      }
-    }, 2000);
-  });
-}
+// function func1() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const error = true;
+//       if (!error) {
+//         console.log("Your Promise has been resolved");
+//         resolve();
+//       } else {
+//         console.log("Your promise has not been resolved");
+//         reject("Sorry not fulfilled");
+//       }
+//     }, 2000);
+//   });
+// }
 
-func1()
-  .then(function () {
-    console.log("Thanks for resolving");
-  })
-  .catch(function () {
-    console.log("Very bad");
-  });
+// func1()
+//   .then(function () {
+//     console.log("Thanks for resolving");
+//   })
+//   .catch(function () {
+//     console.log("Very bad");
+//   });
+
+/////////////////////////////Async , Await/////////////////////////////////////
+
+const whereAmI = async function (country) {
+  const res = await fetch(`
+https://restcountries.com/v3.1/name/${country}`);
+  const data = await res.json();
+  console.log(data);
+};
+
+whereAmI("portugal");
+console.log("first");
